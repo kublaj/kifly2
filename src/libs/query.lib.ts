b0b519e4ae51677beeec6ff6dbaa5cc9b5fc9265
Pimpli Bence <pimplibence@Pimplis-MacBook-Pro.local>
2017-12-10 18:17:27 +0100

    Fix query parser

--- src/libs/query.lib.ts
+++ src/libs/query.lib.ts
@@ -1,9 +1,22 @@
 import * as querystring from 'querystring';
-import { resolve, URL } from 'url';
+import { parse, resolve } from 'url';
 
 export class QueryLib {
     public static parse?(url: string): any {
-        const parsed = new URL(resolve('http://a.b', url));
-        return querystring.parse(parsed.searchParams.toString());
+        const parsed = QueryLib.getParsed(resolve('http://a.b', url));
+        return querystring.parse(parsed);
+    }
+
+    private static getParsed(url: string): string {
+        /*
+        const under6 = semver.lt(process.version, '7.0.0');
+
+        if (under6) {
+            return parse(url).query.toString();
+        } else {
+            return parse(url).query.toString();
+        }
+         */
+        return parse(url).query.toString();
     }
 }
